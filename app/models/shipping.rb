0bc898139c000e845f2aab6da28ff3f143c3600f
TejaKavallappa <tejasbk@gmail.com>
2016-07-04 23:19:17 -0700

    Model validation tests

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,4 +1,4 @@
 class Shipping < ActiveRecord::Base
-  validates :first_name, :last_name, presence: {message: "Please check if column is present"}
-  validates :address, :city, :state, :zip_code, presence: {message: "Please check if column is present"}
+  validates :first_name, :last_name, presence: {message: "can't be blank"}
+  validates :address, :city, :state, :zip_code, presence: {message: "can't be blank"}
 end
--- spec/models/shipping_spec.rb
+++ spec/models/shipping_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Shipping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Shipping, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe Shipping do
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:zip_code) }
 end
